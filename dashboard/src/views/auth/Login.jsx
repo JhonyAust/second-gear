4780d292a0ca05fc8e1e2f34b9a739afdd968dfd
Jhony <jhonyaustcse37@gmail.com>
2023-10-31 17:21:00 +0600

    Admin Login and redux toolkit setup

--- dashboard/src/views/auth/Login.jsx
+++ dashboard/src/views/auth/Login.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AiOutlineGooglePlus, AiOutlineGithub } from 'react-icons/ai'
 import { FiFacebook } from 'react-icons/fi'
 import { CiTwitter } from 'react-icons/ci'
 
 const Login = () => {
+    const [state, setSatate] = useState({
+        email: '',
+        password: ''
+    })
+    const inputHandle = (e) => {
+        setSatate({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(state)
+    }
   return (
     <div className='min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center'>
             <div className='w-[350px] text-[#d0d2d6] p-2'>
                 <div className='bg-[#283046] p-4 rounded-md'>
-                    <h2 className='text-xl mb-3'>Welcome to e-commerce</h2>
+                    <h2 className='text-xl mb-3'>Welcome to Second Gear</h2>
                     <p className='text-sm mb-3'>Please signin to your account and start your bussiness</p>
-                    <form >
+                    <form onSubmit={submit}>
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="email">Email</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="text" name='email' placeholder='email' id='email' required />
+                            <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="text" name='email' placeholder='email' id='email' required />
                         </div>
                         <div className='flex flex-col w-full gap-1 mb-5'>
                             <label htmlFor="password">Password</label>
-                            <input  className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="password" name='password' placeholder='password' id='password' required />
+                            <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="password" name='password' placeholder='password' id='password' required />
                         </div>
                         <button className='bg-blue-500 w-full hover:shadow-blue-500/20 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
                             {
