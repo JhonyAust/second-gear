c9c8ce537aa8607f41e512bd05d3c79605d448a8
Jhony <jhonyaustcse37@gmail.com>
2023-11-01 14:34:07 +0600

    Some css design changes have been made

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -22,23 +22,23 @@ const Register = () => {
         console.log(state)
     }
   return (
-    <div className='min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center'>
-    <div className='w-[350px] text-[#d0d2d6] p-2'>
-        <div className='bg-[#283046] p-4 rounded-md'>
+    <div className='min-w-screen min-h-screen bg-[#2E054E] flex justify-center items-center'>
+    <div className='w-[350px] text-[#FFFFFF] p-2'>
+        <div className='bg-[#561381] p-4 rounded-md'>
             <h2 className='text-xl mb-3'>Welcome to Second Gear</h2>
             <p className='text-sm mb-3'>Please register to your account and start your bussiness</p>
             <form onSubmit={submit}>
                 <div className='flex flex-col w-full gap-1 mb-3'>
                     <label htmlFor="name">Name</label>
-                    <input onChange={inputHandle} value={state.name} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="text" name='name' placeholder='name' id='name' required />
+                    <input onChange={inputHandle} value={state.name} className='px-3 py-2 outline-none border border-violet-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-violet-400 overflow-hidden' type="text" name='name' placeholder='name' id='name' required />
                 </div>
                 <div className='flex flex-col w-full gap-1 mb-3'>
                     <label htmlFor="email">Email</label>
-                    <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="email" name='email' placeholder='email' id='email' required />
+                    <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-violet-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-violet-400 overflow-hidden' type="email" name='email' placeholder='email' id='email' required />
                 </div>
                 <div className='flex flex-col w-full gap-1 mb-3'>
                     <label htmlFor="password">Password</label>
-                    <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="password" name='password' placeholder='password' id='password' required />
+                    <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-violet-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-violet-400 overflow-hidden' type="password" name='password' placeholder='password' id='password' required />
                 </div>
                 <div className='flex items-center w-full gap-3 mb-3'>
                     <input className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-100 rounded border-gray-300 focus:ring-blue-500' type="checkbox" name='checkbox' id='checkbox' required />
